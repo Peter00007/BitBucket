161f31b6e9a75dda6dc2ac76d8a2f321c8a9be19
Peter <petro004>
2019-02-18 17:50:18 +0200

    Commit version 0.1 BitBacket

---
+++ src/main/java/com/bitbucket/BitBucketRunner.java
@@ -0,0 +1,12 @@
+package main.java.com.bitbucket;
+
+/**
+ * Created by User on 16.02.2019.
+ */
+public class BitBucketRunner {
+    public static void main(String[] args) {
+        BitBucketLogic bitBucketLogic = new BitBucketLogic();
+        bitBucketLogic.addValueByPosition();
+        bitBucketLogic.inputArray();
+    }
+}
